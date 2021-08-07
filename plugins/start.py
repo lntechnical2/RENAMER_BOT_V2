e737b93dedc3672d228bb757898f680f6c401f71
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-07 11:12:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,14 +7,7 @@ from helper.database import  insert
 async def start(client,message):
 	insert(int(message.chat.id))
 	await message.reply_text(text =f"""
-	Hello {message.from_user.first_name }
-	__I am file renamer bot, Please sent any telegram 
-	**Document Or Video** and enter new filenameto rename it__
-	""",reply_to_message_id = message.message_id ,  
-	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
-
+	Hai , {message.from_user.first_name } I Am A Rename Bot V2"""
 
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
@@ -25,7 +18,7 @@ async def send_doc(client,message):
        filesize = humanize.naturalsize(file.file_size)
        fileid = file.file_id
        await message.reply_text(
-       f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
+       f"""**Choose A Desired Option** \n**File Name** : `{filename}` \n**File Size** : `{filesize}`"""
        ,reply_to_message_id = message.message_id,
-       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
-       ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
+       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Rename ",callback_data = "rename")
+       ,InlineKeyboardButton("CANCEL",callback_data = "cancel")  ]]))
