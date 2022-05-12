4163218fc272649330a5c987590c771b69c39081
mr_lokaman <82169706+lntechnical2@users.noreply.github.com>
2022-05-12 19:45:37 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,1 +1,7 @@
 
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.private & filters.command(["about"]))
+async def start(client,message):
+	await message.reply_text("Bot :- @rename_urbot\nCreater :- @mrlokaman\Language :-Python3\nLibrary :- Pyrogram 1.4.16\nServer :- Heroku")
